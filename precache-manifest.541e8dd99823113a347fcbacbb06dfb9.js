ba87c62ac8ef53587b794c514f67ea381f0e791e
Alessio Burgio <alessioburgio85@gmail.com>
2020-08-15 18:04:30 +0200

    Updates

---
+++ precache-manifest.541e8dd99823113a347fcbacbb06dfb9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10a3b44ca25613b529e042bf7a9f13e6",
+    "url": "/alan-ai-newsreader-react/index.html"
+  },
+  {
+    "revision": "5d8eaafb1c97d5b9f4a1",
+    "url": "/alan-ai-newsreader-react/static/css/main.e23e6b48.chunk.css"
+  },
+  {
+    "revision": "760e5cc4db17a2778a3b",
+    "url": "/alan-ai-newsreader-react/static/js/2.775fa560.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/alan-ai-newsreader-react/static/js/2.775fa560.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d8eaafb1c97d5b9f4a1",
+    "url": "/alan-ai-newsreader-react/static/js/main.51b69138.chunk.js"
+  },
+  {
+    "revision": "5668fc7bd1839410f1dd",
+    "url": "/alan-ai-newsreader-react/static/js/runtime-main.ba60fffb.js"
+  },
+  {
+    "revision": "d3e2c60f739f623c94466d07f33c8a47",
+    "url": "/alan-ai-newsreader-react/static/media/alan-logo.d3e2c60f.jpg"
+  },
+  {
+    "revision": "33047336aa1a768763a267aa650a9814",
+    "url": "/alan-ai-newsreader-react/static/media/news-placeholder-738.33047336.png"
+  }
+]);
